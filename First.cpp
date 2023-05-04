739d683cc7de9b4dfc6cb4f5920a494c66c8fe39
Sajib Biswas <sajibbiswas@Sajibs-MacBook-Air.local>
2023-05-04 23:19:34 +0600

    pari na kisui

--- First.cpp
+++ First.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 using namespace std;
 int main(){
-    cout << "Hello World" ;
-    cout << "hello woirl";
+    cout << "Hello World" << endl;
+    cout << "hello woirl" << endl;
+    cout << "hi my name is sajib biswas";
 }
